9ff4e83be94766121c2fc8f016bfc27bab1217da
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-08-09 19:14:33 +0530

    completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -103,5 +103,44 @@ router.get("/films/:filmId", function(req, res){
        res.send("The film id doesn't match any movie")
 })
 
+
+////////////////////////////////////////////////////////////////////
+ // Your route code will look like this
+ router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    let n = arr.pop()
+    let sumOfNumbers = [n*(arr[0]+arr[arr.length-1])]/2
+    let sumOfArray = 0;
+    for(let i = 0; i < arr.length; i++){
+        sumOfArray = sumOfArray + arr[i]
+    }
+
+    let missingNumber = sumOfNumbers - sumOfArray
+    ///LOGIC WILL GO HERE 
+	   res.send(  { data: missingNumber  }  );
+ });
+
+
+ ///////////////////////////////////////////////////////
+ router.get("/sol2", function (req, res){
+    let arr= [33, 34, 35, 37, 38]
+    let n = arr.length + 1;
+
+    let sumOfNumbers = [n*(arr[0]+arr[arr.length-1])]/2
+    let sumOfArray = 0;
+    for(let i = 0; i < arr.length; i++){
+        sumOfArray = sumOfArray + arr[i]
+    }
+
+    let missingNumber = sumOfNumbers - sumOfArray; 
+
+    res.send(  { data: missingNumber  }  );
+
+ })
+
+
+
+
 module.exports = router;
 // adding this comment for no reason
