e8063701fd2c241a08d25d4209d291373788b615
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-08-10 13:23:24 +0530

    routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,46 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+///////////////////////////////////////////////////////////////////////////////////
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+    let arr = req.players
+    let newPlayer = req.body.data
+    arr.push(newPlayer)
+    
+    res.send(  { data: players , status: true }  )
+})
+
+
 module.exports = router;
