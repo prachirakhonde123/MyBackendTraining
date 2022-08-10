055eada1f8301d75ee930cc4c612a6c0fb6ebdc4
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-08-10 14:51:47 +0530

    Player Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -73,40 +73,47 @@ router.post("/test-post-4", function(req, res) {
 ///////////////////////////////////////////////////////////////////////////////////
 
 let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ]
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ]
-       },
-   ]
+[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
 
    router.post('/players', function (req, res) {
-    let arr = req.players
-    let newPlayer = req.body.data
-    arr.push(newPlayer)
+    
+    let newPlayer = req.body
+
+    for(let i = 0; i < players.length; i++){
+        let playerName = players[i] 
+        if(newPlayer.name === playerName.name){
+            res.send("Player is already exist!")
+        }
+    }
+    players.push(newPlayer)
     
     res.send(  { data: players , status: true }  )
 })
