04b1ffbe2a0d965880123485ec4778476e8db520
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-08-13 18:00:04 +0530

    Voting and bookingID Assignment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://PrachiRakhonde:TidE9uPBxvyZRFOn@cluster0.vdm2ccj.mongodb.net/Prachi_db?retryWrites=true&w=majority",{
+    useNewUrlParser: true
+})
+
+.then( () => console.log("MondoDB connected") )
+.catch(err => console.log(err))
+
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
