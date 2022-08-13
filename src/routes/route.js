46f4ac1737f6381c6ed72b0e66942630ddf28126
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-08-13 20:05:51 +0530

    route

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+//const UserController= require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createUser", async function(req,res){
+    let dataOfBook = req.body
+    let mydata = await 
+}  )
 
 router.get("/getUsersData", UserController.getUsersData)
 
